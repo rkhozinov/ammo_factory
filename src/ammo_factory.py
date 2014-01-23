aebe666b1b556625576df075facbf095a57085c9
vkhlyuenv <vkhlyunev@mirantis.com>
2014-01-23 14:11:08 +0400

    Some fixes

--- src/ammo_factory.py
+++ src/ammo_factory.py
@@ -1,3 +1,4 @@
+import json
 from keystoneclient.v2_0 import client
 
 AMMO_TMPL = '''\
@@ -22,16 +23,16 @@ def auth(username, password, tenant_name, auth_url):
 def gen_request(method, url, host, headers, body=None):
     assert isinstance(headers,dict)
     assert method.upper() in ['GET', 'POST']
-    body = str(body) if body else ''
+    body = json.dumps(body) if body else ''
 
-    # headers['Content-Type'] = 'application/json'
-    # headers['Content-length'] = str(len(body))
-    # headers['Accept'] = 'application/xml'
+    headers['Content-Type'] = 'application/json'
+    headers['Content-length'] = str(len(body))
+    headers['Accept'] = 'application/xml'
 
     ammo = AMMO_TMPL.format(
         method=method.upper(), host=host, url=url,
         headers='\r\n'.join("%s: %s" % (n, v) for (n, v) in headers.items()),
-        body=(body.replace("'",'"') + '\r\n') if body else '' )
+        body=(body.replace("'", '"') + '\r\n') if body else '' )
     return '%s\n%s' % (len(ammo), ammo)
 
 
--- tmp/experiments.py
+++ tmp/experiments.py
@@ -1,3 +1,5 @@
+import json
+
 r = """GET /v2.0/extensions HTTP/1.1\r
 Host: 172.18.173.130:5000\r
 Content-Type: application/json\r
@@ -5,6 +7,18 @@ Accept: application/xml\r
 \r
 """
 
-with open("1ammo.txt","w") as f:
-            f.write(str(len(r)) + '\n')
-            f.write(r)
+# with open("1ammo.txt","w") as f:
+#             f.write(str(len(r)) + '\n')
+#             f.write(r)
+
+body = {
+            "user": {
+                "username": "jqsmith",
+                "email": "john.smith@example.org",
+                "enabled": 'true',
+                "OS-KSADM:password": "secrete"
+            }
+        }
+j = json.dumps(body)
+print j, len(j)
+print str(j), len(str(j))
